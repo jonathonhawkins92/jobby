0fd1db04e27dcba9be2faec5662d63c7d36f6d36
Goblin <jonathonhawkins92@gmail.com>
2023-04-01 16:24:34 +0100

    add some debug output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,28 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-	return (
-		<main>
-			<h1 className={inter.className}>
-				💩 💩 💩
-			</h1>
-		</main>
-	);
+import { prisma } from "@/../prisma/db";
+
+async function getJobs() {
+    return prisma.job.findMany();
 }
+
+export default async function Home() {
+    const jobs = await getJobs();
+
+    return (
+        <main>
+            <h1 className={inter.className}>💩 💩 💩</h1>
+            <section>
+                <ul>
+                    {jobs.map((j) => (
+                        <li key={j.id}>
+                            <pre>{JSON.stringify(j, null, 4)}</pre>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </main>
+    );
+}
+
