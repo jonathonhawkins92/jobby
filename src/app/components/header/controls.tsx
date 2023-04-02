6b470a91ebebaad9a0bdac3648a570c6d9576506
Goblin <jonathonhawkins92@gmail.com>
2023-04-02 16:20:49 +0100

    only show profile when logged in

--- src/app/components/header/controls.tsx
+++ src/app/components/header/controls.tsx
@@ -7,7 +7,9 @@ import { ThemeToggle } from "./theme-toggle";
 
 function UserAvatar() {
     const router = useRouter();
-    const { user } = useUser();
+    const { user, isSignedIn } = useUser();
+
+    if (!isSignedIn) return null;
 
     return (
         <div className="h-8 w-8 flex-none rounded-full border bg-white p-0.5 align-middle text-sm font-medium text-slate-700 shadow-sm transition-all hover:bg-slate-50 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white dark:border-slate-700 dark:bg-slate-900 dark:text-slate-400 dark:hover:bg-slate-800 dark:hover:text-white dark:focus:ring-offset-slate-800">
