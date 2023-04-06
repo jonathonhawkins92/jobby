4a9d9656ecbd3273ec4386bdeb11d4cb21dd44d6
Goblin <jonathonhawkins92@gmail.com>
2023-04-06 23:38:23 +0100

    Switching to the most basic middleware

--- src/app/companies/[id]/page.tsx
+++ src/app/companies/[id]/page.tsx
@@ -1,4 +1,23 @@
-export default function Company() {
-    return <div>hi</div>;
+import type { Company } from "@prisma/client";
+
+async function getCompanyData(id: string) {
+    const res = await fetch(`http://localhost:3000/api/company/${id}`, {
+        method: "GET",
+        headers: {
+            accepts: "application/json",
+        },
+    });
+
+    return res.json() as Promise<Company>;
+}
+
+export default async function Company({ params }: { params: { id: string } }) {
+    const data = await getCompanyData(params.id);
+
+    return (
+        <code>
+            <pre>{JSON.stringify(data, null, 4)}</pre>
+        </code>
+    );
 }
 
