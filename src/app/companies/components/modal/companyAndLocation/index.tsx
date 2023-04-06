a32d4c8bd06cb8b1dc30010c1605edc8931e8c46
Goblin <jonathonhawkins92@gmail.com>
2023-04-06 15:05:39 +0100

    de-nesting location form so keyboard events don't bubble

--- src/app/companies/components/modal/companyAndLocation/index.tsx
+++ src/app/companies/components/modal/companyAndLocation/index.tsx
@@ -17,7 +17,7 @@ import { useToggle } from "~/hooks/useToggle";
 import clsx from "clsx";
 import type { CompanyAndLocation } from "~/app/api/companyAndLocation/model";
 import type { Location } from "~/app/api/location/model";
-import type { PropsWithChildren } from "react";
+import type { PropsWithChildren, FormEvent } from "react";
 
 function ChipButton({
     children,
@@ -70,38 +70,17 @@ function Chip({
 
 function LocationField({
     isDisabled = false,
-    onSubmit,
-    locationNames = [],
-    onLocationRemove,
-    onLocationEdit,
-    name,
-    address,
-    city,
-    country,
-    region,
-    selectedIndex,
-    onClose,
+    names = [],
+    onRemove,
+    onEdit,
+    onAdd,
 }: {
     isDisabled?: boolean;
-    onSubmit: (location: Location, index?: number) => void;
-    locationNames?: string[];
-    onLocationRemove: (index: number) => void;
-    onLocationEdit: (index: number) => void;
-    name?: Location["name"];
-    address?: Location["address"];
-    city?: Location["city"];
-    country?: Location["country"];
-    region?: Location["region"];
-    selectedIndex?: number;
-    onClose: () => void;
+    names?: string[];
+    onRemove: (index: number) => void;
+    onEdit: (index: number) => void;
+    onAdd: () => void;
 }) {
-    const [isLocationModalOpen, setIsLocationModalOpen] = useToggle(false);
-
-    function handleLocationModalClose() {
-        setIsLocationModalOpen(false);
-        onClose();
-    }
-
     return (
         <>
             <Label>Locations</Label>
@@ -109,19 +88,16 @@ function LocationField({
                 <Button
                     disabled={isDisabled}
                     variant="flatIcon"
-                    onClick={() => setIsLocationModalOpen(true)}
+                    onClick={() => onAdd()}
                 >
                     <AddIcon />
                 </Button>
                 <ol className="inline-flex flex-wrap gap-4">
-                    {locationNames.map((name, index) => (
+                    {names.map((name, index) => (
                         <li key={index}>
                             <Chip
-                                onEdit={() => {
-                                    onLocationEdit(index);
-                                    setIsLocationModalOpen(true);
-                                }}
-                                onRemove={() => onLocationRemove(index)}
+                                onEdit={() => onEdit(index)}
+                                onRemove={() => onRemove(index)}
                                 isDisabled={isDisabled}
                             >
                                 {name}
@@ -130,100 +106,35 @@ function LocationField({
                     ))}
                 </ol>
             </div>
-            {isLocationModalOpen && (
-                <Dialog
-                    onMouseDown={handleLocationModalClose}
-                    onTouchStart={handleLocationModalClose}
-                >
-                    <DialogClickBarrier className="rounded-md border-2 border-purple-300 bg-white shadow-md shadow-purple-300/90 dark:border-blue-300/40 dark:bg-slate-900 dark:text-white dark:shadow-blue-300/40 sm:max-w-[50%]">
-                        <LocationForm
-                            isDisabled={isDisabled}
-                            onSubmit={(location) => {
-                                onSubmit(location, selectedIndex);
-                                handleLocationModalClose();
-                            }}
-                            onClose={handleLocationModalClose}
-                            submitButtonText={
-                                selectedIndex !== undefined ? "Update" : "Add"
-                            }
-                            name={name}
-                            address={address}
-                            city={city}
-                            country={country}
-                            region={region}
-                        />
-                    </DialogClickBarrier>
-                </Dialog>
-            )}
         </>
     );
 }
+
 function Form({
     onClose,
     isDisabled = false,
     onSubmit,
+    onLocationAdd,
+    onLocationRemoval,
+    onLocationEdit,
     submitButtonText = "Create",
+    locationNames = [],
 }: {
     onClose: () => void;
     isDisabled?: boolean;
-    onSubmit: (company: CompanyAndLocation) => void;
+    onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+    onLocationAdd: () => void;
+    onLocationRemoval: (index: number) => void;
+    onLocationEdit: (index: number) => void;
     submitButtonText?: string;
+    locationNames?: string[];
 }) {
-    const [locationNames, setLocationNames] = useState<string[]>([]);
-    const [selectedLocationIndex, setSelectedLocationIndex] = useState<
-        undefined | number
-    >();
-    const methods = useForm<CompanyAndLocation>({
-        defaultValues: defaultCompanyAndLocation,
-    });
-    const { getValues, setValue, handleSubmit, setFocus } = methods;
-
-    function handleLocationAddition(location: Location, index?: number) {
-        const locations = getValues("location");
-        if (index !== undefined) {
-            locations.splice(index, 1, location);
-        } else {
-            locations.push(location);
-        }
-        setValue("location", locations);
-        setLocationNames(locations.map((l) => l.name));
-        setFocus("name");
-    }
-
-    function handleLocationRemoval(index: number) {
-        const locations = getValues("location");
-        locations.splice(index, 1);
-        setValue("location", locations);
-        setLocationNames(locations.map((l) => l.name));
-    }
-
-    function handleLocationEdit(index: number) {
-        setSelectedLocationIndex(index);
-    }
-
-    function handleLocationDeselect() {
-        setSelectedLocationIndex(undefined);
-        setFocus("name");
-    }
-
-    const selectedLocation = useMemo(() => {
-        if (selectedLocationIndex === undefined) {
-            return;
-        }
-        const locations = getValues("location");
-        const location = locations[selectedLocationIndex];
-        if (location === undefined) {
-            return;
-        }
-        return location;
-    }, [getValues, selectedLocationIndex]);
-
     return (
         <form
             className=" flex max-h-screen flex-col"
-            onSubmit={(e) => {
-                e.preventDefault();
-                void handleSubmit(onSubmit)(e);
+            onSubmit={(event) => {
+                event.preventDefault();
+                onSubmit(event);
             }}
         >
             <header className="border-common-color flex items-end justify-between border-b-[1px] p-4">
@@ -237,23 +148,14 @@ function Form({
                 </Button>
             </header>
             <article className="flex grow flex-wrap gap-4 overflow-y-auto p-4">
-                <FormProvider {...methods}>
-                    <CompanyFields isDisabled={isDisabled} />
-                    <LocationField
-                        isDisabled={isDisabled}
-                        onSubmit={handleLocationAddition}
-                        locationNames={locationNames}
-                        onLocationRemove={handleLocationRemoval}
-                        onLocationEdit={handleLocationEdit}
-                        name={selectedLocation?.name}
-                        address={selectedLocation?.address}
-                        city={selectedLocation?.city}
-                        country={selectedLocation?.country}
-                        region={selectedLocation?.region}
-                        selectedIndex={selectedLocationIndex}
-                        onClose={handleLocationDeselect}
-                    />
-                </FormProvider>
+                <CompanyFields isDisabled={isDisabled} />
+                <LocationField
+                    isDisabled={isDisabled}
+                    names={locationNames}
+                    onRemove={onLocationRemoval}
+                    onEdit={onLocationEdit}
+                    onAdd={onLocationAdd}
+                />
             </article>
             <footer className="border-common-color flex justify-end border-t-[1px] p-4">
                 <Submit value={submitButtonText} disabled={isDisabled} />
@@ -269,11 +171,13 @@ export function Modal() {
         setIsCompanyModalOpen(false);
     }
 
+    const [isLocationModalOpen, setIsLocationModalOpen] = useToggle(false);
+
     const router = useRouter();
     const [isPending, startTransition] = useTransition();
     const [isFetching, setIsFetching] = useState(false);
 
-    async function handleSubmit(data: CompanyAndLocation) {
+    async function handleSubmitToServer(data: CompanyAndLocation) {
         setIsFetching(true);
         // Mutate external data source
         await fetch("/api/companyAndLocation", {
@@ -296,8 +200,53 @@ export function Modal() {
 
     const isMutating = isFetching || isPending;
 
+    const [locationNames, setLocationNames] = useState<string[]>([]);
+    const [selectedLocationIndex, setSelectedLocationIndex] = useState<
+        undefined | number
+    >();
+
+    const methods = useForm<CompanyAndLocation>({
+        defaultValues: defaultCompanyAndLocation,
+    });
+    const { getValues, setValue, handleSubmit, setFocus } = methods;
+
+    function handleLocationAddition(location: Location, index?: number) {
+        const locations = getValues("location");
+        if (index !== undefined) {
+            locations.splice(index, 1, location);
+        } else {
+            locations.push(location);
+        }
+        setValue("location", locations);
+        setLocationNames(locations.map((l) => l.name));
+    }
+
+    function handleLocationRemoval(index: number) {
+        const locations = getValues("location");
+        locations.splice(index, 1);
+        setValue("location", locations);
+        setLocationNames(locations.map((l) => l.name));
+    }
+
+    function handleLocationModalClose() {
+        setIsLocationModalOpen(false);
+        setFocus("name");
+    }
+
+    const selectedLocation = useMemo(() => {
+        if (selectedLocationIndex === undefined) {
+            return;
+        }
+        const locations = getValues("location");
+        const location = locations[selectedLocationIndex];
+        if (location === undefined) {
+            return;
+        }
+        return location;
+    }, [getValues, selectedLocationIndex]);
+
     return (
-        <>
+        <FormProvider {...methods}>
             <Button
                 disabled={isMutating}
                 variant="flatIcon"
@@ -314,14 +263,57 @@ export function Modal() {
                         <Form
                             onClose={handleCompanyModalClose}
                             isDisabled={isMutating}
-                            onSubmit={(companyAndLocation) =>
-                                void handleSubmit(companyAndLocation)
+                            onSubmit={(event) =>
+                                void handleSubmit(handleSubmitToServer)(event)
                             }
+                            onLocationAdd={() => {
+                                setIsLocationModalOpen(true);
+                            }}
+                            onLocationEdit={(index) => {
+                                setSelectedLocationIndex(index);
+                                setIsLocationModalOpen(true);
+                            }}
+                            onLocationRemoval={handleLocationRemoval}
+                            locationNames={locationNames}
                         />
                     </DialogClickBarrier>
                 </Dialog>
             )}
-        </>
+            {isLocationModalOpen && (
+                <Dialog
+                    onMouseDown={handleLocationModalClose}
+                    onTouchStart={handleLocationModalClose}
+                >
+                    <DialogClickBarrier className="rounded-md border-2 border-purple-300 bg-white shadow-md shadow-purple-300/90 dark:border-blue-300/40 dark:bg-slate-900 dark:text-white dark:shadow-blue-300/40 sm:max-w-[50%]">
+                        <LocationForm
+                            isDisabled={isMutating}
+                            onSubmit={(location) => {
+                                handleLocationAddition(
+                                    location,
+                                    selectedLocationIndex
+                                );
+                                handleLocationModalClose();
+                                setSelectedLocationIndex(undefined);
+                            }}
+                            onClose={() => {
+                                handleLocationModalClose();
+                                setSelectedLocationIndex(undefined);
+                            }}
+                            submitButtonText={
+                                selectedLocationIndex !== undefined
+                                    ? "Update"
+                                    : "Add"
+                            }
+                            name={selectedLocation?.name}
+                            address={selectedLocation?.address}
+                            city={selectedLocation?.city}
+                            country={selectedLocation?.country}
+                            region={selectedLocation?.region}
+                        />
+                    </DialogClickBarrier>
+                </Dialog>
+            )}
+        </FormProvider>
     );
 }
 
