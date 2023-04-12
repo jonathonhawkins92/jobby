65e23b385cfcf7d367b491d04abfc3a776b2a5c5
Goblin <jonathonhawkins92@gmail.com>
2023-04-12 11:15:22 +0100

    Add init to Resp classes

--- src/utils/server/response.ts
+++ src/utils/server/response.ts
@@ -1,46 +1,40 @@
-import { NextResponse } from "next/server";
-
-export class Unauthorized extends NextResponse {
-    constructor() {
+export class Unauthorized extends Response {
+    constructor(init: ResponseInit = {}) {
         super("Unauthorized", {
             status: 401,
+            ...init,
         });
     }
 }
 
-export class Created extends NextResponse {
-    constructor(response?: Record<string, unknown> | BodyInit | null) {
-        if (typeof response === "object") {
-            super(JSON.stringify(response), {
-                status: 201,
-            });
-        } else {
-            super(response, {
-                status: 201,
-            });
-        }
+export class Created extends Response {
+    constructor(body?: Record<string, unknown>, init: ResponseInit = {}) {
+        const payload = body ? JSON.stringify(body) : "Created";
+        super(payload, { status: 201, ...init });
     }
 }
 
-export class ServerError extends NextResponse {
-    constructor() {
+export class ServerError extends Response {
+    constructor(init: ResponseInit = {}) {
         super("Internal Server Error", {
             status: 500,
+            ...init,
         });
     }
 }
 
-export class Invalid extends NextResponse {
-    constructor() {
+export class Invalid extends Response {
+    constructor(init: ResponseInit = {}) {
         super("Invalid Request", {
             status: 400,
+            ...init,
         });
     }
 }
 
-export class Json extends NextResponse {
-    constructor(response: Record<string, unknown>, status = 200) {
-        super(JSON.stringify(response), { status });
+export class Json extends Response {
+    constructor(response: Record<string, unknown>, init: ResponseInit = {}) {
+        super(JSON.stringify(response), { status: 200, ...init });
     }
 }
 
