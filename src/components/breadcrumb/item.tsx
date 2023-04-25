bcc317cd4ac4bccd5174fb93c24aecb463d64279
Goblin <jonathonhawkins92@gmail.com>
2023-04-25 14:19:29 +0100

    clean up breadcrumb a little

--- src/components/breadcrumb/item.tsx
+++ src/components/breadcrumb/item.tsx
@@ -1,28 +1,16 @@
 import type { PropsWithChildren } from "react";
 
+import { ChevronRight } from "~/components/icons/chevron-right";
+
 export function BreadcrumbItem({
 	children,
 	isCurrent = false,
 }: PropsWithChildren<{ isCurrent?: boolean }>) {
 	return (
-		<li {...(isCurrent ? { ariaCurrent: "page" } : {})}>
+		<li aria-current={isCurrent ? "page" : undefined}>
 			<div className="flex items-center gap-1">
 				<h1>{children}</h1>
-				{isCurrent ? null : (
-					<svg
-						aria-hidden="true"
-						className="h-4 w-4"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				)}
+				{!isCurrent && <ChevronRight />}
 			</div>
 		</li>
 	);
