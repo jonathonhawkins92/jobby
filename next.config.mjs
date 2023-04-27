50b22d9248f5a8d9cffba51b5d8acdcfef7e2b14
Goblin <jonathonhawkins92@gmail.com>
2023-04-27 01:32:38 +0100

    Testing out @t3-oss/env

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
+import "./src/env.mjs";
+
 /** @type {import('next').NextConfig} */
 const config = {
-    experimental: {
-        appDir: true,
-    },
-    reactStrictMode: true,
-    images: {
-        domains: [
-            "images.clerk.dev",
-            "cdn.shopify.com",
-            "www.docker.com",
-            "media.licdn.com", // linkedin :P
-        ],
-    },
+	experimental: {
+		appDir: true,
+	},
+	reactStrictMode: true,
+	images: {
+		domains: [
+			"images.clerk.dev",
+			"cdn.shopify.com",
+			"www.docker.com",
+			"media.licdn.com", // linkedin :P
+		],
+	},
 };
 
 export default config;
-
